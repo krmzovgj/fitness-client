936e3de87d99de4aee58db1bfb6dc31ffe596c9a
Krmzov0 <krmzovgorgi0@gmail.com>
2025-12-24 12:13:43 +0100

    Fix sign in req payload

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,13 +3,15 @@ import { api } from "./axios";
 interface SignInPayload {
     email: string;
     password: string;
-    tenantId: string;
 }
 
-export const signIn = async (data: SignInPayload): Promise<any> => {
+export const signIn = async (
+    data: SignInPayload,
+    tenantId: string
+): Promise<any> => {
     const response = await api.post<any>("/auth/sign-in", data, {
         headers: {
-            tenantId: data.tenantId,
+            tenantId: tenantId,
         },
     });
     return response.data;
