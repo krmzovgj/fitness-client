e8793f5cafd9624ce91aa36e1c3d60fda2b3167a
Krmzov0 <krmzovgorgi0@gmail.com>
2026-01-02 02:18:10 +0100

    Fix mandatory field z index

--- src/components/ui/mandatory-input-wrapper.tsx
+++ src/components/ui/mandatory-input-wrapper.tsx
@@ -7,7 +7,7 @@ interface MandatoryWrapperProps {
 export const MandatoryWrapper = ({ children }: MandatoryWrapperProps) => {
     return (
         <div className="relative">
-            <span className="absolute text-red-500 left-0 -top-2 text-2xl">
+            <span className="absolute z-50 text-red-500 left-0 -top-2 text-2xl">
                 *
             </span>
             {children}
